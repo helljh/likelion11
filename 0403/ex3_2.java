426f90455a815a1f46ce0982fbcf1047746d3438
helljh <97391158+helljh@users.noreply.github.com>
2023-04-08 10:59:46 +0900

    Add files via upload

---
+++ 0403/ex3_2.java
@@ -0,0 +1,13 @@
+package Practice1;
+
+public class ex3_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 0;
+        while(i<=100) {
+            sum += i;
+            i++;
+        }
+        System.out.println("1~100까지의 합: " + sum);
+    }
+}
